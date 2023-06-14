ff9a422b6ca4f813e9421d51658eb0d2cb5fba47
Pawel Damasiewicz <pawel.damasiewicz@gmail.com>
2023-06-14 22:57:47 +0200

    Modify aliases, add laravel sail alias

--- aliases.zsh
+++ aliases.zsh
@@ -3,3 +3,4 @@ tailf() { tail -f "$@" }
 edit() { $EDITOR "$@" }
 dexec() { docker-compose exec "$@" }
 dockdev() { docker-compose -f docker-compose.yml -f docker-compose-dev.yml up -d }
+alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
