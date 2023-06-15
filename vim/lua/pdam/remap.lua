db230ee2305ca0d5e6c0f0b3b7d4a9329517a1f6
Pawel Damasiewicz <pawel.damasiewicz@gmail.com>
2023-06-15 13:56:16 +0200

    Modify remap.lua, add usefull remaps

--- vim/lua/pdam/remap.lua
+++ vim/lua/pdam/remap.lua
@@ -1,2 +1,18 @@
 vim.g.mapleader = ','
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+vim.keymap.set('t', '<Esc>', "<C-\\><C-n>")
+
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+vim.keymap.set('n', '<leader>y', "\"+y")
+vim.keymap.set('v', '<leader>y', "\"+y")
+vim.keymap.set('n', '<leader>Y', "\"+Y")
+
+vim.keymap.set('n', 'Q', '<nop>')
+
+vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
+vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
+vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
+vim.keymap.set('n', '<leader>j', '<cmd>lpext<CR>zz')
