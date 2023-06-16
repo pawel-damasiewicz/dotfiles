0f2627bb24189f91684b97fad8378839c0476234
Pawel Damasiewicz <pawel.damasiewicz@gmail.com>
2023-06-16 11:25:39 +0200

    Modify packer.lua, add zen-mode

--- vim/lua/pdam/packer.lua
+++ vim/lua/pdam/packer.lua
@@ -37,4 +37,5 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
+    use("folke/zen-mode.nvim")
 end)
